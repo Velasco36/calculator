a23fe814159a68ce2f2519118d93116ee5ba6314
V3lasco36 <migue16velasco@gmail.com>
2023-03-08 15:24:26 -0400

    fix:  agregando la funcion calcular

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 window.addEventListener('load', () => {
 
     //se guarda el elemento que se necesiaa del html
-    const display = document.querySelector('.calculator')
+    const display = document.querySelector('.ans')
+  
+
     const buttons = document.getElementsByClassName ('cal-button')
 
     //convertir el htmlcollection a arraay
@@ -10,10 +12,48 @@ window.addEventListener('load', () => {
 
     //Iteramos en los botones
     keypadButtonsArray.forEach(( button) =>{
-        //a cada boton le agragamos un listener
+        //a cada boton le agragamos un listener 
         button.addEventListener('click', ()=> {
-            console.log(button.value)
+
+        //console.log(display.value, "display")
+        //console.log(button.value, "boton" )
+        //console.log(button, "todo del boton")
+        calculadora(button, display)
+          
+          
         })
     })
 
 });
+
+
+
+
+function calculadora(button, display){
+    switch(button.value){
+        
+        case 'C':
+            borrar(display);
+            break;
+        case '=':
+            calcular(display);
+            break;
+
+        default:
+            actualizar(display, button);
+            break;
+    }
+}
+
+
+function calcular(display){
+    console.log(display.value);
+
+    try {
+    
+    display.value = eval(display.value);
+    }catch(e){
+        display.value ='ERROR';
+    }
+
+}

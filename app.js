e75b7bc57a1d0819f43c80f45f2f97c871703d28
V3lasco36 <migue16velasco@gmail.com>
2023-03-08 12:06:30 -0400

    fix:agregando la funcion click a los botos

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
 window.addEventListener('load', () => {
+
+    //se guarda el elemento que se necesiaa del html
     const display = document.querySelector('.calculator')
     const buttons = document.getElementsByClassName ('cal-button')
 
+    //convertir el htmlcollection a arraay
     const keypadButtonsArray = Array.from(buttons);
 
+    //Iteramos en los botones
     keypadButtonsArray.forEach(( button) =>{
-        console.log(button)
+        //a cada boton le agragamos un listener
+        button.addEventListener('click', ()=> {
+            console.log(button.value)
+        })
     })
 
 });

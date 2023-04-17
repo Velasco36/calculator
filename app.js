3061809071096ec13c46880314ba355343c29b82
Gian vespa <givespas@gmail.com>
2023-04-17 14:18:58 -0500

    fix<app>: logica

--- app.js
+++ app.js
@@ -1,131 +1,68 @@
+const screen = document.getElementById('screen');
+
+// manipular botones en pantalla
 window.addEventListener('load', () => {
-  //se guarda el elemento que se necesiaa del html
-  const display = document.querySelector('.ans');
-  
-
-  const buttons = document.getElementsByClassName('cal-button');
-
-    //convertir el htmlcollection a arraay
-    const keypadButtonsArray = Array.from(buttons);
-
-    //Iteramos en los botones
-    keypadButtonsArray.forEach(( button) =>{
-        //a cada boton le agragamos un listener 
-        button.addEventListener('click', ()=> {
-          console.log(display)
-
-       
-        calculadora(button, display)
-          
-          
-        })
-    })
-
+  Array.from(document.getElementsByClassName('cal-button')).forEach(
+    (button) => {
+      button.addEventListener('click', () => {
+        switch (button.value) {
+          case 'AC':
+            screen.value = '';
+            break;
+          case '=':
+            calcular();
+            break;
+          default:
+            actualizar(button);
+            break;
+        }
+      });
+    }
+  );
 });
 
-// tarea validar que solo reciba numeros
-window.addEventListener('keypress', printNumber);
-function printNumber(e) {
-
-  const screen = document.getElementById('screen');
-  const code = e.charCode
-  const number = e.key
-
-  console.log(number)
-  
-  
-  //console.log(screen)
- 
-
-  if (number === 'Backspace'){
-    console.log('borrar')
+// Solo para ingresar numeros
+window.addEventListener('keypress', function (e) {
+  const code = e.charCode;
+  const number = e.key;
+  if (code >= 42 && code < 58) {
+    screen.value += number;
+    return;
   }
-  // || code ==127 || code ==13 || code ==32 || code ==98
-  if (code === 127){
-    
-    screen.value = value.substring(0, value.length -1)
+});
+
+// validar teclas
+window.addEventListener('keydown', function (e) {
+  const keyboard = e.key;
+
+  if (keyboard === 'Backspace') {
+    return (screen.value = screen.value.substring(0, screen.value.length - 1));
   }
 
-  if (code === 13 && screen.value !== ''){
-    return calcular(screen) 
-  }
-  //calcular Enter
-  if (code === 32){
-    return calcular(screen, number);
-  }
-  //Borrar 
-  if (code ===98){
-    screen.value = screen.value.substring(0, screen.value.length -1)
+  if (keyboard === 'Delete') {
+    screen.value = '';
   }
 
-  //validar que solo se puedan colocar numero
-  if (code >=42 && code <=58)  {
-    
-    screen.value += number
+  if (keyboard === 'Enter' && screen.value !== '') {
+    return calcular();
   }
-  if (code ===37){
-    screen.value += number
+
+  if (keyboard === '%') {
+    return (screen.value += number);
   }
- 
-}  
- 
-//Errores no puedes usar el teclado y los numeros de la calculadora al mism tiempo suma los dos numeros por teclado y deja en el display los numeros que indroduciste  con su teclado
+});
 
-//bug con ac y el teclado nomal 
-
-
-function calculadora(button, display) {
-
-  console.log(button.value)
-  
-
-  switch (button.value) {
-  
-    case 'AC':
-      //console.log('precionando borrar')
-      //borrar(display);
-
-      borrar(display, button);
-      break;
-    case '=':
-      calcular(display);
-      break;
-
-    default:
-      actualizar(display, button);
-      break;
-  }
-}
-
-function calcular(display) { 
-
+function calcular() {
   try {
-    display.value = eval(display.value);
-    
-  
+    screen.value = eval(screen.value);
   } catch (e) {
-    display.value = 'ERROR';
+    screen.value = 'ERROR';
   }
 }
 
-
-function actualizar(display, button) {
-  if (display.value === 0) {
-    display.value = ' ';
+function actualizar(button) {
+  if (screen.value === 0) {
+    screen.value = ' ';
   }
-
-  display.value += button.value;
-}
-
-function borrar(display, button) {
- 
-  
-  
-    display.value = '';
-    //button.value = ''
-    display.value += display.value
-    
-  
-  
-  
+  screen.value += button.value;
 }
